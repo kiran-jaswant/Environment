4e0af92d1602885bf8b60ef8715800cdb8301905
kiran-jaswant <kiranjaswant08@gmail.com>
2024-05-01 13:04:03 +0530

    Basic folder structure created

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+     <h1 className='bg-slate-400 p-5 text-center text-md '>Environment Project</h1>
+    </>
+  )
+}
+
+export default App
